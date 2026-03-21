70fc49d951b4e886e6488b3ef74ca4bcef4ee9eb
Ubuntu <ubuntu@ip-172-31-34-214.us-east-2.compute.internal>
2026-03-21 20:54:19 +0000

    ajustes en servidor

--- apps/api/src/db/prisma.ts
+++ apps/api/src/db/prisma.ts
@@ -10,6 +10,6 @@ if (!process.env.DATABASE_URL) {
 }
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
+const adapter = new PrismaPg(pool as any);
 
 export const prisma = new PrismaClient({ adapter });
